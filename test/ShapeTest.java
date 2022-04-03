45c6dc42aa4c77cbfe7b12f27c406b2df664a99e
Raimon Bleda-Vilalta <rai.bleda@gmail.com>
2022-04-03 17:15:57 -0400

    test changes

--- test/ShapeTest.java
+++ test/ShapeTest.java
@@ -2,7 +2,12 @@ import org.junit.Before;
 import org.junit.Test;
 
 import java.awt.Color;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
+import model.AnimatorModel;
+import model.EasyAnimatorModel;
 import model.Oval;
 import model.Posn;
 import model.Rect;
@@ -89,6 +94,65 @@ public class ShapeTest {
     shape.changeShapeDimensions(5, 7);
     assertEquals(5, shape.getHeight());
     assertEquals(7, shape.getWidth());
+  }
+
+  @Test
+  public void testGetLogAddShape() {
+    AnimatorModel m = new EasyAnimatorModel();
+    m.addShape(shape.getShapeType(), shape.getHeight(), shape.getWidth(), shape.getColor(),
+            shape.getShapePosn(), shape.getShapeID(), 2, 3);
+    List<String> str = new ArrayList<>(List.of("shape s RECTANGLE 10 10 0 0 255 1 1 2 3"));
+    assertEquals(m.getShapes().get(0).getLog(), str);
+  }
+
+  @Test
+  public void testAddMultipleShapes() {
+    AnimatorModel m = new EasyAnimatorModel();
+    m.addShape(shape.getShapeType(), shape.getHeight(), shape.getWidth(), shape.getColor(),
+            shape.getShapePosn(), shape.getShapeID(), 2, 3);
+    Shape shape2 = new Oval(5, 5, Color.RED,
+            new Posn(50, 50),"oval1", ShapeType.OVAL);
+    m.addShape(shape2.getShapeType(), shape2.getHeight(), shape2.getWidth(), shape2.getColor(),
+            shape2.getShapePosn(), shape2.getShapeID(), 1, 2);
+    List<String> str1 = new ArrayList<>(List.of("shape s RECTANGLE 10 10 0 0 255 1 1"));
+    List<String> str2 = new ArrayList<>(List.of("shape oval1 OVAL 5 5 255 0 0 50 50"));
+    assertEquals(m.getShapes().get(0).getLog(), str1);
+    assertEquals(m.getShapes().get(1).getLog(), str2);
+  }
+
+  @Test
+  public void testGetLogAddAndMove() {
+    AnimatorModel m = new EasyAnimatorModel();
+    m.addShape(shape.getShapeType(), shape.getHeight(), shape.getWidth(), shape.getColor(),
+            shape.getShapePosn(), shape.getShapeID(), 1, 4);
+    m.moveShape(2, 3, shape.getShapePosn(),
+            new Posn(100, 100), shape.getShapeID());
+    List<String> str = new ArrayList<>(Arrays.asList("shape s RECTANGLE 10 10 0 0 255 1 1 1 4",
+            "motion move s 2 1 1 10 10 0 0 255 3 100 100 10 10 0 0 255"));
+    assertEquals(m.getShapes().get(0).getLog(), str);
+  }
+
+  @Test
+  public void testGetLogAddandChangeColor() {
+    AnimatorModel m = new EasyAnimatorModel();
+    m.addShape(shape.getShapeType(), shape.getHeight(), shape.getWidth(), shape.getColor(),
+            shape.getShapePosn(), shape.getShapeID(), 1, 4);
+    m.changeColor(shape.getShapeID(), 1, 3, Color.BLUE, Color.RED);
+    List<String> str = new ArrayList<>(Arrays.asList("shape s RECTANGLE 10 10 0 0 255 1 1 1 4",
+            "motion color s 1 1 1 10 10 0 0 255 3 1 1 10 10 255 0 0"));
+    assertEquals(m.getShapes().get(0).getLog(), str);
+  }
+
+  @Test
+  public void testGetLogAddandChangeSize() {
+    AnimatorModel m = new EasyAnimatorModel();
+    m.addShape(shape.getShapeType(), shape.getHeight(), shape.getWidth(), shape.getColor(),
+            shape.getShapePosn(), shape.getShapeID(), 1, 4);
+    m.changeSize(shape.getShapeID(), 1, 3, 10,
+            10, 15, 15);
+    List<String> str = new ArrayList<>(Arrays.asList("shape s RECTANGLE 10 10 0 0 255 1 1 1 4",
+            "motion size s 1 1 1 10 10 0 0 255 3 1 1 15 15 0 0 255"));
+    assertEquals(m.getShapes().get(0).getLog(), str);
   }
 
 }
--- test/TextViewTest.java
+++ test/TextViewTest.java
@@ -1,8 +1,11 @@
 import org.junit.Before;
 import org.junit.Test;
 
+import java.awt.*;
+import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Scanner;
 
 import io.AnimationBuilder;
 import io.AnimationFileReader;
@@ -10,6 +13,8 @@ import io.TweenModelBuilder;
 import io.ViewsFactory;
 import model.AnimatorModel;
 import model.EasyAnimatorModel;
+import model.Posn;
+import model.ShapeType;
 import view.AnimatorTextView;
 import view.IView;
 
@@ -25,13 +30,25 @@ public class TextViewTest {
 
   @Test
   public void testRenderAnimation() throws IOException {
-    TweenModelBuilder<AnimatorModel> modelBuilder = new AnimationBuilder(m);
-    AnimationFileReader reader = new AnimationFileReader();
-    reader.readFile("buildings.txt", modelBuilder);
-    AnimatorModel model = modelBuilder.build();
+    //TweenModelBuilder<AnimatorModel> modelBuilder = new AnimationBuilder(m);
+    //AnimationFileReader reader = new AnimationFileReader();
+    //reader.readFile("buildings.txt", modelBuilder);
+    //AnimatorModel model = modelBuilder.build();
+    int sceneWidth = 200;
+    int sceneHeight = 250;
+    m.buildScene(sceneWidth, sceneHeight);
+    m.addShape(ShapeType.RECTANGLE, 10, 15, new Color(0, 0, 0),
+            new Posn(100, 125), "rect1", 1, 5);
+    m.moveShape(2, 3, new Posn(100, 125),
+            new Posn(150, 150), "rect1");
+    File file = new File("TextViewTest");
     IView textView = new AnimatorTextView(m, "TextViewTest", 10);
     textView.renderAnimation();
-    FileReader fr = new FileReader("TextViewTest");
-    assertEquals(str, "");
+    Scanner scan = new Scanner(file);
+    String canvas = scan.next();
+    assertEquals(canvas, "canvas");
+    assertEquals(scan.nextInt(), sceneWidth);
+    assertEquals(scan.nextInt(), sceneHeight);
+
   }
 }
