8dcc5db1923b88acb7ddc082087d0c3d417560a9
John Buergler <buergler.j@northeastern.edu>
2022-04-04 15:39:29 -0400

    view javadoc

--- src/view/IView.java
+++ src/view/IView.java
@@ -9,6 +9,14 @@ public interface IView {
    * When prompting the main method, user can declare the output type that they prefer. If no
    * file exists with the given file name, the method will create a file in the given format that
    * contains the animation information.
+   * </p>
+   * SVG view is outputted in standard SVG format. Creates a rect or an ellipse and animates for
+   * a given time.
+   * </p>
+   * Text view is outputted in this format for motion : "motion startTime xPosStart yPosStart width
+   * height red green blue endTime xPosEnd yPosEnd widthEnd heightEnd redEnd greenEnd blueEnd".
+   * and this format for adding a shape : "shape ShapeID ShapeType height width red green blue xPos
+   * yPos startOfLife endOfLife".
    */
   public void renderAnimation() throws IOException;
 
