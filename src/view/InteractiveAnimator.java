fc71174d1a8f163f5d6e87d242dd7238fced97c6
John Buergler <buergler.j@northeastern.edu>
2022-04-11 21:30:20 -0400

    interface methods

--- src/view/InteractiveAnimator.java
+++ src/view/InteractiveAnimator.java
@@ -4,9 +4,9 @@ import model.AnimatorModel;
 
 public class InteractiveAnimator extends AnimatorGraphicsView implements InteractiveGraphicsView {
   /**
-   * The constructor for the AnimatorGraphicsView. Takes in a model and a speed and then creates
-   * an animation from the information contained. Check AnimatorModel for more information on
-   * the storage of the animation.
+   * The constructor for the InteractiveAnimator. Takes in a model and a speed and then creates
+   * an animation from the information contained. the same as the original graphics view, plus
+   * other functionality including speed up, slow down, toggle loopback, pause, play, and rewind.
    *
    * @param model AnimatorModel that contains user-generated animation.
    * @param speed The speed of the animation being played in ticks per second.
@@ -15,5 +15,28 @@ public class InteractiveAnimator extends AnimatorGraphicsView implements Interac
     super(model, speed);
   }
 
+  @Override
+  public void changeSpeed(int speed) {
 
+  }
+
+  @Override
+  public void toggleLoopback() {
+
+  }
+
+  @Override
+  public void pause() {
+
+  }
+
+  @Override
+  public void play() {
+
+  }
+
+  @Override
+  public void restart() {
+
+  }
 }
--- src/view/InteractiveGraphicsView.java
+++ src/view/InteractiveGraphicsView.java
@@ -1,4 +1,14 @@
 package view;
 
 public interface InteractiveGraphicsView {
+
+  public void changeSpeed(int speed);
+
+  public void toggleLoopback();
+
+  public void pause();
+
+  public void play();
+
+  public void restart();
 }
